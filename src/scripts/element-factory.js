69e3114c5dc6c3e40cffa205647d03935e269934
Sarvesh Suryakant Arondekar <115332581+sarvesh11030801@users.noreply.github.com>
2023-11-05 12:44:55 +0530

    first commit

---
+++ src/scripts/element-factory.js
@@ -0,0 +1,31 @@
+class ElementFactory {
+  constructor(tag) {
+    this.el = document.createElement(tag);
+  }
+  text(val) {
+    this.el.appendChild(document.createTextNode(val));
+    return this;
+  }
+  class(val) {
+    this.el.classList.add(val);
+    return this;
+  }
+  attribute(key, val) {
+    this.el.setAttribute(key, val);
+    return this;
+  }
+  addTo(parent) {
+    parent.appendChild(this.el);
+    return this.el;
+  }
+  insertBefore(parent, ref) {
+    parent.insertBefore(this.el, ref);
+    return this.el;
+  }
+}
+
+const element = (tag) => {
+  return new ElementFactory(tag);
+};
+
+export default element;
